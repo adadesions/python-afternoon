accfaf4afd98592d0946aa91d67d022dbf25d86a
adadesions <adadesions@gmail.com>
2020-10-24 15:49:54 +0700

    4th

--- rock_paper_scissors2.py
+++ rock_paper_scissors2.py
@@ -7,16 +7,34 @@ fullname = {
     's': 'Scissors'
 }
 bot = ['r', 'p', 's'] # 0:r, 1:p, 2:s
+score = {
+    'player': 0,
+    'bot': 0,
+    'draw': 0
+}
 
 while True:
     player = input("Rock [r], Paper [p], Scissors [s], Exit [x]: ")
 
     if player == 'x':
         print("Thanks for playing, see ya next time!")
+        print("===== Score Board =====")
+        print("Player:",score['player'])
+        print("Bot:",score['bot'])
+        print("Draw:",score['draw'])
+        print("=======================")
+        print("The Champion: ", "Player" if score['player'] > score['bot'] else 'Bot')
+        print("=======================")
+
         break
 
-    print("You choose:", fullname[player])
+    if len(player) > 1:
+        continue
+    
+    if player not in 'rsp' or player == '':
+        continue
 
+    print("You choose:", fullname[player])
     botChoose = bot[random.randint(0, 2)]
     print('Bot:', fullname[botChoose])
 
@@ -26,9 +44,12 @@ while True:
 
     if decision == 'rs' or decision == 'pr' or decision == 'sp':
         winner = 'Player'
+        score['player'] += 1
     elif decision == 'rp' or decision == 'ps' or decision == 'sr':
         winner = 'Bot'
+        score['bot'] += 1
     else:
         winner = 'Draw'
+        score['draw'] += 1
 
     print("The winner is the", winner)
