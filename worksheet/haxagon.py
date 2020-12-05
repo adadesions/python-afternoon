14a377a72d9c2e8b914ab7a863f4d39ce405ee3d
adadesions <adadesions@gmail.com>
2020-12-05 15:00:59 +0700

    Q4

--- worksheet/haxagon.py
+++ worksheet/haxagon.py
@@ -1,1 +1,25 @@
-https://github.com/adadesions/python-afternoon
+# Question 4
+
+from turtle import *
+
+bgcolor('black')
+pencolor('yellow')
+width(5)
+
+for i in range(6):
+    forward(100)
+    left(360/6)
+
+left(360/6)
+forward(200)
+
+for i in range(4):
+    if i % 2 == 0:
+        side = 100
+    else:
+        side = 200
+
+    left(2*360/6)
+    forward(side)
+
+done()
