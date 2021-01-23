6604f29b4fb1282a9c3b27504d89184c96d444e8
adadesions <adadesions@gmail.com>
2021-01-23 14:16:46 +0700

    Mubashir

--- worksheet/2021/mubashir.py
+++ worksheet/2021/mubashir.py
@@ -1,10 +1,27 @@
 # Mubashir.py
 
 def mubashir_cipher(message):
+    # List
     key = [['m', 'c'], ['u', 'e'], ['b', 'g'], ['a', 'k'], 
     ['s', 'v'], ['h', 'x'], ['i', 'z'], ['r', 'y'], 
     ['p', 'w'], ['l', 'n'], ['o', 'j'], ['t', 'f'], ['q', 'd']]
-    print('Ada')
+
+    encode = ''
+    for letter in message:
+        found = False
+        for k in key:
+            if letter == k[1]:
+                encode += k[0]
+                found = True
+
+            elif letter == k[0]:
+                encode += k[1]
+                found = True
+
+        if not found:
+            encode += letter
+    
+    print(encode)
 
 
 mubashir_cipher("mubashir is not amazing")
